cab7ec8c776c10193ad81601611072603c213069
Alex Fotopoulos <alex.foto.dev@gmail.com>
2022-05-31 15:23:14 -0400

    add toggle to Nav.js to collapse menu when links are clicked and modify paragraph style on Homepage.js

--- client/src/components/Nav.js
+++ client/src/components/Nav.js
@@ -18,11 +18,22 @@ export default function Nav() {
     //state to determine toggle light or dark
     const [isDarkMode, setIsDarkMode] = useState(null)
 
+    //state to handle toggling of navbar menu
+    const [isCollapsed, setIsCollapsed] = useState(true)
+
     //change refresh in favorite context so Results page will refresh when switching favorites from nav bar
     function handleToggleRefresh() {
         favoritesCtx.toggleRefresh()
+        setIsCollapsed(true)
     }
 
+    //function to toggle navbar menu collapse
+    function handleCollapseMenu() {
+        setIsCollapsed(true)
+    }
+    function handleShowMenu() {
+        setIsCollapsed(false)
+    }
     //function to toggle light/dark mode
     function toggleMode() {
         if (themeCtx.theme === "dark") {
@@ -42,10 +53,10 @@ export default function Nav() {
     //create Links in dropdown for favorited teams
     let content
     if (!authCtx.token) {
-        content = <li>Login to add favorites</li>
+        content = <li className="dropdownLink">Login to add favorites</li>
     } else {
         if (favoritesCtx.favorites.length === 0) {
-            content = <li>You have no favorites</li>
+            content = <li className="dropdownLink">You have no favorites</li>
         } else {
             content = favoritesCtx.favorites.map(team => (
                 <Link className="dropdownLink" onClick={handleToggleRefresh} key={team._id} to={`/teams/${team.name}`}>{team.name}</Link>
@@ -56,10 +67,10 @@ export default function Nav() {
         <nav className={`navbar navbar-expand-md navbar-${themeCtx.theme === "light" ? "light" : "dark"} bg-${themeCtx.theme === "light" ? "light" : "dark"} sticky-top mb-4`}>
             <div className="container-fluid">
                 <Link className="navbar-brand" to="/">Stattrac</Link>
-                <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+                <button className={isCollapsed ? "navbar-toggler collapsed" : "navbar-toggler"} onClick={handleShowMenu} type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
                     <span className="navbar-toggler-icon"></span>
                 </button>
-                <div className="collapse navbar-collapse" id="navbarNavDropdown">
+                <div className={isCollapsed ? "collapse navbar-collapse" : "collapse navbar-collapse show"} id="navbarNavDropdown">
                     <ul className="navbar-nav">
                         <li className="nav-item dropdown">
                             <a className="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" role="button" data-bs-toggle="dropdown" aria-expanded="false">
@@ -72,17 +83,17 @@ export default function Nav() {
                             </ul>
                         </li>
                         <li className="nav-item">
-                            <a className="nav-link" href="https://www.pro-football-reference.com/" target="_blank">Pro Football Reference</a>
+                            <a className="nav-link" href="https://www.pro-football-reference.com/" target="_blank" onClick={handleCollapseMenu}>Pro Football Reference</a>
                         </li>
                     </ul>
                     <ul className="navbar-nav Nav-authButtons">
-                        {!authCtx.token && <li className="nav-item">
+                        {!authCtx.token && <li className="nav-item" onClick={handleCollapseMenu}>
                             <Link className="nav-link" to="/users/register">Register</Link>
                         </li>}
-                        {!authCtx.token && <li className="nav-item">
+                        {!authCtx.token && <li className="nav-item" onClick={handleCollapseMenu}>
                             <Link className="nav-link" to="/users/login">Login</Link>
                         </li>}
-                        {authCtx.token && <li className="nav-item">
+                        {authCtx.token && <li className="nav-item" onClick={handleCollapseMenu}>
                             <Link onClick={logout} className="nav-link" to="/">Logout</Link>
                         </li>}
                     </ul>
